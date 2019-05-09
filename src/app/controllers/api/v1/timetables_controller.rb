0f1203c6b29a9b9982f34872d159dbfc0967975f
sho <sskmy1024.y@gmail.com>
2019-05-09 14:52:55 +0900

    make timetable endpoint

--- src/app/controllers/api/v1/timetables_controller.rb
+++ src/app/controllers/api/v1/timetables_controller.rb
@@ -1,9 +1,24 @@
+require 'time'
+require 'date'
+
 class Api::V1::TimetablesController < ApplicationController
 
-  def index
-    @timetables = TimetableSet..timetables.where("course_id = ? AND departure_time >= ?", [コース情報], Time.now ).limit(3)
+  def index(datetime=DateTime.now.to_s, limit=5)
+    @date = Date.parse(datetime)
+    @time = Time.parse(datetime)
+    @limit = limit
 
+    @course = Course.find_by(departure_id: params[:from], arrival_id: params[:to])
 
-    render json: { success: false }, status: :ok
+    @timetables = DateSet.find_by(date: @date).timetable_set.timetables.where("course_id = ? AND departure_time >= ?", @course.id, @time ).limit(@limit)
+
+    render json: { success: true, timetable: @timetables, course: JSON.parse(@course.to_json(:include => [:arrival, :departure]) ) }, status: :ok
   end
+
+  private
+
+  def get_timetable_params(params)
+    params.permit(:key, :from, :to, :datetime, :limit)
+  end
+
 end
