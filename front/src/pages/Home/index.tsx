8ad9fb5cf4b81406ac7fee6990808ba3722e007b
sho <sskmy1024.y@gmail.com>
2019-08-17 12:37:06 +0900

    install bootstrap

---
+++ front/src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Button } from "react-bootstrap";
+
+interface HomeProps {
+  name: String;
+}
+
+function Home({ name }: HomeProps) {
+  return <Button variant="primary">{name}</Button>;
+}
+
+export default Home;
