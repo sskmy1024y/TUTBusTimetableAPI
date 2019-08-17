2015464cb54711c68310f777f2bbef235926d098
sho <sskmy1024.y@gmail.com>
2019-08-18 00:58:56 +0900

    add prettier

---
+++ front/prettier.config.js
@@ -0,0 +1,12 @@
+'use strict'
+
+module.exports = {
+  trailingComma: 'none',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  printWidth: 100,
+  proseWrap: 'always',
+  endOfLine: 'lf'
+}
