08f2a653e945cf6a192fa61600c1b6fd5db98335
sho <sskmy1024.y@gmail.com>
2019-08-18 00:04:45 +0900

    create header

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,27 +1,34 @@
 import React from "react";
+
 import logo from "./logo.svg";
+
+import Header from "./components/Header";
 import Home from "./pages/Home/";
+
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Home name="test" />
-    </div>
+    <>
+      <Header />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <Home name="test" />
+      </div>
+    </>
   );
 }
 
---
+++ front/src/components/Header/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { Container, Navbar, Nav } from "react-bootstrap";
+
+import styled from "styled-components";
+
+export default function Header() {
+  return (
+    <header>
+      <Container>
+        <Navbar bg="light" expand="lg" role="navigation" fixed="top">
+          <Container>
+            <Navbar.Brand href="#home">TUT Bus Timetable API</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">API Doc</Nav.Link>
+                <Nav.Link href="#link">Contact</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </Container>
+    </header>
+  );
+}
