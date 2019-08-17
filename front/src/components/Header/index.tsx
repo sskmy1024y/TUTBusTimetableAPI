476423693f8c0503ea6de3abdd0303c70b9dbdab
sho <sskmy1024.y@gmail.com>
2019-08-18 00:59:43 +0900

    formatted prettier

--- front/src/components/Header/index.tsx
+++ front/src/components/Header/index.tsx
@@ -1,25 +1,25 @@
-import React from "react";
+import React from 'react'
 
-import { Container, Navbar, Nav } from "react-bootstrap";
+import { Container, Navbar, Nav } from 'react-bootstrap'
 
 export default function Header() {
   return (
     <header>
       <Container>
-        <Navbar bg="light" expand="lg" role="navigation" fixed="top">
+        <Navbar bg='light' expand='lg' role='navigation' fixed='top'>
           <Container>
-            <Navbar.Brand href="#home">TUT Bus Timetable API</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="mr-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">API Doc</Nav.Link>
-                <Nav.Link href="#link">Contact</Nav.Link>
+            <Navbar.Brand href='#home'>TUT Bus Timetable API</Navbar.Brand>
+            <Navbar.Toggle aria-controls='basic-navbar-nav' />
+            <Navbar.Collapse id='basic-navbar-nav'>
+              <Nav className='mr-auto'>
+                <Nav.Link href='#home'>Home</Nav.Link>
+                <Nav.Link href='#link'>API Doc</Nav.Link>
+                <Nav.Link href='#link'>Contact</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
         </Navbar>
       </Container>
     </header>
-  );
+  )
 }
