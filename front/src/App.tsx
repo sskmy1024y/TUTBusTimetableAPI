5b1e4a34c20e52b07fa5f9998760a7efbf8cd25e
sho <sskmy1024.y@gmail.com>
2019-08-18 00:58:56 +0900

    add prettier

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,35 +1,35 @@
-import React from "react";
+import React from 'react'
 
-import logo from "./logo.svg";
+import logo from './logo.svg'
 
-import Header from "./components/Header";
-import Home from "./pages/Home/";
+import Header from './components/Header'
+import Home from './pages/Home/'
 
-import "./App.css";
+import './App.css'
 
 function App() {
   return (
     <>
       <Header />
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
           <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             Learn React
           </a>
         </header>
-        <Home name="test" />
+        <Home name='test' />
       </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
