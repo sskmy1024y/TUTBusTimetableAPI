70220bba9680c9ab1f50eba6736e353658b23685
sho <sskmy1024.y@gmail.com>
2019-09-12 02:02:30 +0900

    fix docker-files

--- front/src/middleware/FetchTimetable/index.ts
+++ front/src/middleware/FetchTimetable/index.ts
@@ -9,7 +9,7 @@ interface FetchTimetablePayload {
 export const fetchTimetable = (payload: FetchTimetablePayload): ThunkActionType => async (
   dispatch: Dispatch<Action>
 ) => {
-  await fetch(`${process.env.REACT_APP_API_HOST}/api/v1/timetables/internal?datetime=${'2019-10-24 10:00'}`, {
+  await fetch(`${process.env.REACT_APP_DEV_API_HOST}/api/v1/timetables/internal?datetime=${'2019-10-24 10:00'}`, {
     mode: 'cors',
   })
     .then(response => {
