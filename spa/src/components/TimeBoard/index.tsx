b8048ec226cf00859deb6cd944a2c075bcc2c654
sho <sskmy1024.y@gmail.com>
2019-10-22 21:12:50 +0900

    add no bust view

--- spa/src/components/TimeBoard/index.tsx
+++ spa/src/components/TimeBoard/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { TimetableCollectType } from '../../modules/Timetable/type'
-import TimeBoardItem from './TimeBoardItem'
+import TimeBoardItem, { NoBusItem } from './TimeBoardItem'
 
 interface TimeBoardProps {
   timetables: TimetableCollectType[]
@@ -10,9 +10,13 @@ interface TimeBoardProps {
 export default function TimeBoard({ timetables }: TimeBoardProps) {
   return (
     <>
-      {timetables.map((timetable, index) => {
-        return <TimeBoardItem key={index} timetable={timetable} />
-      })}
+      {timetables.length > 0 ? (
+        timetables.map((timetable, index) => {
+          return <TimeBoardItem key={index} timetable={timetable} />
+        })
+      ) : (
+        <NoBusItem />
+      )}
     </>
   )
 }
