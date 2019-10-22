39b00ee9f94461da12290ac6565e0deda75b20cd
sho <sskmy1024.y@gmail.com>
2019-10-22 15:21:22 +0900

    fix routing

--- api/app/controllers/static_pages_controller.rb
+++ api/app/controllers/static_pages_controller.rb
@@ -5,12 +5,4 @@ class StaticPagesController < ApplicationController
   def index
     render file: 'public/index.html', layout: false
   end
-
-  def document
-    
-  end
-
-  def contact
-    
-  end
 end
