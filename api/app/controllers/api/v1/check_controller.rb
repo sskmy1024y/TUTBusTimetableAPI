85677a690cdbf525c1bbb5a59794b2656829abc9
sho <sskmy1024.y@gmail.com>
2019-11-14 03:13:41 +0900

    update slack bot style

--- api/app/controllers/api/v1/check_controller.rb
+++ api/app/controllers/api/v1/check_controller.rb
@@ -6,8 +6,10 @@ class Api::V1::CheckController < ApplicationController
   before_action :basic_auth
   protect_from_forgery
 
+  HOST_URL = 'http://www.teu.ac.jp'
+
   def index
-    url = ENV.fetch('CHECK_TIMETABLE_UPDATE', '')
+    url = ENV.fetch('CHECK_TIMETABLE_URL', '')
     result_internal_error("Could not parse html in nokogiri") if url.empty?
 
     charset = nil
@@ -18,40 +20,73 @@ class Api::V1::CheckController < ApplicationController
     doc = Nokogiri::HTML.parse(html, charset)
     result_internal_error("Could not parse html in nokogiri") if doc.blank?
 
-    htmldata = doc.css('.detailBox01.m-txt-ttl5').to_s
-    md5 = Digest::MD5.new.update(htmldata).to_s
+    container = doc.css('.accessstyle')
+    md5 = Digest::MD5.new.update(container.to_s).to_s
 
-    last_detect_hash = DetectChangeTimetable.last()&.uuid
-    if md5 == last_detect_hash
+    last_detect_data = DetectChangeTimetable.last()
+    if md5 != last_detect_data&.uuid
       return not_detected_update
     else
-      last_detect_hash = DetectChangeTimetable.create(
-        uuid: md5
-      )
+      # last_detect_data = DetectChangeTimetable.create(
+      #   uuid: md5
+      # )
+
+      # 掲載されているリンクリストを作成
+      dividers = []
+      container.css('ul').each do | divider |
+        divider_links = []
+        divider_title = divider.css('li[data-role="list-divider"]').inner_text
+        divider.css('a').each do | link |
+          divider_links << {
+            title: link.inner_text,
+            link: link.attribute("href").value
+          }
+        end
+        dividers << {
+          divider: divider_title,
+          links: divider_links,
+        }
+      end
 
       slack_webhook_url = ENV.fetch('WEBHOOK_URL', nil)
       unless slack_webhook_url.nil?
-        notifier = Slack::Notifier.new(slack_webhook_url, username: "時刻表更新Bot")
-        attachments = {
-          color: "#FF8C00",
-          pretext: "時刻表掲載ページの更新を検知しました。",
-          title: "スクールバス時刻表",
-          title_link: "https://www.teu.ac.jp/campus/access/006644.html#bustimetable",
-          text: "掲載情報を確認して、更新内容に応じて時刻表を登録してください。",
-          fields: [
-            {
-              title: "Detection Hash",
-              value: last_detect_hash.uuid
+        
+        attachments = [
+          {
+            color: "success",
+            pretext: "時刻表掲載ページの更新を検知しました。",
+            title: "スクールバス時刻表",
+            title_link: url,
+            fields: [
+              {
+                title: "Detection Hash",
+                value: last_detect_data.uuid
+              }
+            ],
+            footer: "Detection Bot",
+            footer_icon: "https://platform.slack-edge.com/img/default_application_icon.png",
+            ts: last_detect_data.created_at.to_i
+          }
+        ]
+
+        dividers.each do | divider |
+          divider[:links].each_with_index do | link, index |
+            attachments << {
+              color: "#FF8C00",
+              pretext: index == 0 ? divider[:divider] : "",
+              title: link[:title],
+              title_link: "#{request.protocol}#{request.host_with_port}/register/new?url=#{HOST_URL}#{link[:link]}",
+              text: link[:link]
             }
-          ],
-          footer: "Detection Bot",
-          footer_icon: "https://platform.slack-edge.com/img/default_application_icon.png",
-          ts: last_detect_hash.created_at.to_i
-        }
-        notifier.post attachments: [attachments]
+          end
+        end
+
+        notifier = Slack::Notifier.new(slack_webhook_url, username: "時刻表更新Bot")
+        notifier.post attachments: attachments
       end
 
-      render json: { success: true, data: last_detect_hash.uuid }, status: :ok
+      # render json: { success: true, data: last_detect_data.uuid }, status: :ok
+      render json: { success: true, data: dividers }, status: :ok
     end
   end
 
