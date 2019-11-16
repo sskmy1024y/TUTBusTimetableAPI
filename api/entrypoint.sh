6748a712519621e5c88148c825f21efe0f45f4fb
sho <sskmy1024.y@gmail.com>
2019-11-16 10:51:42 +0900

    add multi stage build

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 set -e
 
-bundle install
-
-# Remove a potentially pre-existing server.pid for Rails.
-rm -f /myapp/tmp/pids/server.pid
-
-bundle exec whenever -i
-service cron start
+if [ $RAILS_ENV == 'production' ]; then
+  service cron start
+else
+  bundle install
+fi
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
